eba1b4d7fa34ad6bcb5713b97aeeaaeaa9203e24
muratkoyun <mkoyun@red.software.systems>
2024-09-12 16:04:20 +0200

    base css features added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-
-  </div>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia" />
+      <h1>Pinia TODO App</h1>
+    </header>
+    <div class="inputField">
+      <input type="text" placeholder="I need to.." />
+      <button>Add</button>
+    </div>
+  </main>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
